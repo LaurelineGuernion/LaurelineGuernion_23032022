97a2ec22665dec0e2d4127a4c420efe081810292
Guernion Laureline <guernion.laureline@gmail.com>
2021-10-21 15:43:32 +0200

    création BDD models sequelize

---
+++ backend/app.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+const mysql = require('mysql');
+require('dotenv').config()
+
+module.exports = app;
---
+++ backend/migrations/20211013141124-create-user.js
@@ -0,0 +1,52 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nom: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      prenom: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      mdp: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      photo: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      bio: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      isAdmin: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
