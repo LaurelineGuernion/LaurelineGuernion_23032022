e6f7f0ae69ab2899f654165bb44418947f05809e
Guernion Laureline <guernion.laureline@gmail.com>
2021-11-15 15:28:41 +0100

    Modification de app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,45 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+require('dotenv').config({path: './config/.env'});
+const cors = require('cors');
+const helmet = require("helmet");
+const xss = require('xss-clean');
+const { Sequelize } = require('sequelize');
+
 const app = express();
-const mysql = require('mysql');
-require('dotenv').config()
+
+app.use(express.json ({limite: '10kb'}));
+app.use(cors());
+app.use(helmet());
+app.use(xss())
+
+// tester la connexion Sequelize avec la base de données
+const sequelize = new Sequelize(process.env.DEV_DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    dialect: "mysql",
+    host: "localhost"
+});
+
+try {
+    sequelize.authenticate();
+    console.log('Connecté à la base de données MySQL!');
+  } catch (error) {
+    console.error('Impossible de se connecter, erreur suivante :', error);
+};
+
+const userRoutes = require('./routes/user');
+//const postRoutes = require('./routes/post');
+//const commentRoutes = require('./routes/comment');
+
+//////////////////// Récupérer les informations du body ////////////////////
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//////////////////// Pointer vers le repertoire images ////////////////////
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/auth', userRoutes);
+//app.use('/api/posts', postRoutes);
+//app.use('/api/comments', commentRoutes);
 
 module.exports = app;
